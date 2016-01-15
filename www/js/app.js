02f70ede320f475e1396f442b3d2c08e4e6755cc
Ranjeet <ranjeetkumar692@gmail.com>
2016-01-15 17:16:57 +0530

    discovery page

--- www/js/app.js
+++ www/js/app.js
@@ -24,10 +24,29 @@ angular.module('setting', ['ionic','setting.controllers', 'setting.services', 'i
 .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('app', {
-      url: '/app',
-      templateUrl: 'templates/home.html',
-      controller: 'HomeCtrl'
-    });
+        url: '/app',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      })
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'templates/profile.html',
+      })
+      .state('discovery', {
+        url: '/discovery',
+        templateUrl: 'templates/discovery.html',
+      })
+      .state('application', {
+        url: '/application',
+        templateUrl: 'templates/application.html',
+      })
+      .state('chat', {
+        url: '/chat',
+        templateUrl: 'templates/chat.html',
+      }).state('user', {
+        url: '/user',
+        templateUrl: 'templates/user.html',
+      });
 
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app');
